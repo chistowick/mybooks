93ff24f662e25a25f7d4cb9305a995e8a41ba057
Anatoly <chistowick@yandex.ru>
2020-05-03 13:24:58 +0300

    Доработал регулярные выражения маршрутов так, чтобы пользователь с наименьшей вероятностью получал ответ, о том, что какая-то страница не существует, а вместо этого перенаправлялся на подходящую страницу. Также добавил необходимые маршруты в routes.php.

--- config/routes.php
+++ config/routes.php
@@ -1,18 +1,31 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 return array(
+    // A page with publications and a page with a single publication
+    '([\s\S]*)publications/articles/([0-9]{1,3})([\s\S]*)' => 'publications/getOneItem/$2',
+    '([\s\S]*)publications/reviews/([0-9]{1,3})([\s\S]*)' => 'publications/getOneItem/$2',
+    '([\s\S]*)publications([\s\S]*)' => 'publications/getList',
     
-    'publications/articles/([0-9]+)' => 'publications/getOneItem/$1',
-    'publications/reviews/([0-9]+)' => 'publications/getOneItem/$1',
-    'publications' => 'publications/getList',
-    'index.php' => 'publications/getList',
-    '' => 'publications/getList',
+    // Pages with quotes
+    '([\s\S]*)quotes/random-quotes([\s\S]*)' => 'quotes/getRandomQuotes',
+    '([\s\S]*)quotes([\s\S]*)' => 'quotes/getFrontPage',
     
-//    'publications/([a-z]+)/([0-9]+)' => 'publications/getOneItem/$1/$2',
+    // Pages - "what to read"
+    '([\s\S]*)what-to-read/recommendations/by-genre([\s\S]*)' => 'whatToRead/getRecommendations/by-genre',
+//    '([\s\S]*)what-to-read/recommendations/by-author([\s\S]*)' => 'whatToRead/getRecommendations/by-author',
+    '([\s\S]*)what-to-read/by-genre([\s\S]*)' => 'whatToRead/getFrontPage/by-genre',
+//    '([\s\S]*)what-to-read/by-author([\s\S]*)' => 'whatToRead/getFrontPage/by-author',
+    '([\s\S]*)what-to-read([\s\S]*)' => 'whatToRead/getFrontPage/by-genre',
+    
+    // Page "about me"
+    '([\s\S]*)about-me([\s\S]*)' => 'aboutMe/getFrontPage',
+    
+    
+    '([\s\S]*)contacts([\s\S]*)' => 'contacts/getFrontPage',
+    
+    '([\s\S]*)index.php([\s\S]*)' => 'publications/getList',
+    '[\s\S]+' => 'publications/getList',
+    '[\s\S]*' => 'publications/getList',
 );
+
+//    'publications/([a-z]+)/([0-9]+)' => 'publications/getOneItem/$1/$2',

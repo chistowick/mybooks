0cd1fdb2174910614bd5bad5af053a64594f27c7
Anatoly <chistowick@yandex.ru>
2020-05-15 12:27:06 +0300

    Изменил файл с маршрутами согласно новой системе страниц и разделов сайта. Теперь при попытке доступа к несуществующему адресу, должна быть показана страница 404 с соответствующим HTTP кодом ответа пользователю, а не осуществлена перемаршрутизация на существующую страницу как раньше

--- config/routes.php
+++ config/routes.php
@@ -2,30 +2,25 @@
 
 return array(
     // A page with publications and a page with a single publication
-    '([\s\S]*)publications/articles/([0-9]{1,3})([\s\S]*)' => 'publications/getOneItem/$2',
-    '([\s\S]*)publications/reviews/([0-9]{1,3})([\s\S]*)' => 'publications/getOneItem/$2',
-    '([\s\S]*)publications([\s\S]*)' => 'publications/getList',
+    'publications/([0-9]{1,3})' => 'publications/getOneItem/$1',
+    'publications/([0-9]{1,3})' => 'publications/getOneItem/$1',
+    'publications' => 'publications/getList',
     
     // Pages with quotes
-    '([\s\S]*)quotes/random-quotes([\s\S]*)' => 'quotes/getRandomQuotes',
-    '([\s\S]*)quotes([\s\S]*)' => 'quotes/getFrontPage',
+    'quotes' => 'quotes/getFrontPage',
     
     // Pages - "what to read"
-    '([\s\S]*)what-to-read/recommendations/by-genre([\s\S]*)' => 'whatToRead/getRecommendations/by-genre',
-//    '([\s\S]*)what-to-read/recommendations/by-author([\s\S]*)' => 'whatToRead/getRecommendations/by-author',
-    '([\s\S]*)what-to-read/by-genre([\s\S]*)' => 'whatToRead/getFrontPage/by-genre',
-//    '([\s\S]*)what-to-read/by-author([\s\S]*)' => 'whatToRead/getFrontPage/by-author',
-    '([\s\S]*)what-to-read([\s\S]*)' => 'whatToRead/getFrontPage/by-genre',
+    'what-to-read' => 'whatToRead/getFrontPage',
     
     // Page "about me"
-    '([\s\S]*)about-me([\s\S]*)' => 'aboutMe/getFrontPage',
+    'about-me' => 'aboutMe/getFrontPage',
     
+    // Page "contacts"
+    'contacts' => 'contacts/getFrontPage',
     
-    '([\s\S]*)contacts([\s\S]*)' => 'contacts/getFrontPage',
-    
-    '([\s\S]*)index.php([\s\S]*)' => 'publications/getList',
-    '[\s\S]+' => 'publications/getList',
-    '[\s\S]*' => 'publications/getList',
+    // Main page
+    'index.php' => 'publications/getList',
+    '' => 'publications/getList',
 );
 
 //    'publications/([a-z]+)/([0-9]+)' => 'publications/getOneItem/$1/$2',

c298a0796a4137596d34df750abbc561aeee5c1e
Anatoly <chistowick@yandex.ru>
2020-05-15 11:05:26 +0300

    Добавил вид страницы 404

---
+++ views/errors/404.php
@@ -0,0 +1,8 @@
+<?php include_once (ROOT . '/views/main_parts/top_side_index.php'); ?>
+
+<div id="div_404">
+    <p id="num_404">404</p>
+    <p id="text_under_404">такой страницы ещё нет</p>
+</div>
+
+<?php include_once (ROOT . '/views/main_parts/bottom_side_index.php'); ?>

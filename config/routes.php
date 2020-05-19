4360c212dec05b9bf6351ce920dfe40fb8d26f8a
Anatoly <chistowick@yandex.ru>
2020-05-19 16:17:20 +0300

    Изменил регулярные выражения маршрутов так, чтобы фиксировались начало и конец строки, а также добавил исключение нуля в начале идентификатора публикации

--- config/routes.php
+++ config/routes.php
@@ -2,25 +2,25 @@
 
 return array(
     // A page with publications and a page with a single publication
-    'publications/([0-9]{1,3})' => 'publications/getOneItem/$1',
-    'publications/([0-9]{1,3})' => 'publications/getOneItem/$1',
-    'publications' => 'publications/getList',
+    '^publications/([1-9]\\d{0,2})$' => 'publications/getOneItem/$1',
+    '^publications/([1-9]\\d{0,2})$' => 'publications/getOneItem/$1',
+    '^publications$' => 'publications/getList',
     
     // Pages with quotes
-    'quotes' => 'quotes/getFrontPage',
+    '^quotes$' => 'quotes/getFrontPage',
     
     // Pages - "what to read"
-    'what-to-read' => 'whatToRead/getFrontPage',
+    '^what-to-read$' => 'whatToRead/getFrontPage',
     
     // Page "about me"
-    'about-me' => 'aboutMe/getFrontPage',
+    '^about-me$' => 'aboutMe/getFrontPage',
     
     // Page "contacts"
-    'contacts' => 'contacts/getFrontPage',
+    '^contacts$' => 'contacts/getFrontPage',
     
     // Main page
-    'index.php' => 'publications/getList',
-    '' => 'publications/getList',
+    '^index.php$' => 'publications/getList',
+    '^$' => 'publications/getList',
 );
 
 //    'publications/([a-z]+)/([0-9]+)' => 'publications/getOneItem/$1/$2',
